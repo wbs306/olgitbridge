5c6c88219df2850071d159edc9cd64990d4667ae
wbs306 <wbs306@gmail.com>
2025-05-20 19:51:33 +0800

    Now can sync all of user's projects and push to remote repo

---
+++ config.sync.js
@@ -0,0 +1,11 @@
+const config = require('./config');
+
+const syncConfig = module.exports = {};
+
+syncConfig.email = 'CHANGEME';
+
+syncConfig.password = 'CHANGEME';
+
+syncConfig.userDir = config.baseDir + 'users/';
+
+syncConfig.remote = 'CHANGEME'
---
+++ src/syncUser.js
@@ -0,0 +1,70 @@
+const axios = require('axios');
+const fs = require('fs/promises');
+const olops = require('./olops');
+const config = require('../config');
+const syncConfig = require('../config.sync');
+const git = require('./git');
+
+const olServer = config.olServer;
+const port = config.port;
+const blueDir = config.bluesDir;
+
+const email = syncConfig.email;
+const password = syncConfig.password;
+const remote = syncConfig.remote;
+
+const syncProject = async (projectId) => {
+    await axios.get(`http://localhost:${port}/sync-${projectId}`,
+        {
+            auth: {
+                username: email,
+                password: password
+            }
+        }
+    ).then(response => {
+        return response.data;
+    });
+}
+
+const copyFilesToUserFolder = async (projectId, targetDir) => {
+    const projectDir = targetDir + projectId;
+    try {
+        await fs.stat(projectDir);
+    } catch (e) {
+        if (e.code !== 'ENOENT') throw e;
+        console.log(projectDir)
+        await fs.mkdir(projectDir);
+    }
+
+    await fs.cp(blueDir + projectId, projectDir, { recursive: true });
+}
+
+const main = async () => {
+    const client = olops.client();
+
+    await olops.login(client, olServer, email, password);
+    const userId = await olops.getProjectPage(client, olServer, '');
+    const userDir = syncConfig.userDir + userId + '/';
+
+    try {
+        await fs.stat(userDir);
+    } catch (e) {
+        if (e.code !== 'ENOENT') throw e;
+        await fs.mkdir(userDir, { recursive: true });
+        await git.createRepo(remote, userDir);
+    }
+
+    try {
+        const projects = await olops.userProjects(client, olServer);
+        for (const project of projects) {
+            const projectId = project._id;
+            await syncProject(projectId);
+            await copyFilesToUserFolder(projectId, userDir);
+        }
+        await git.save('', userDir, 'Synced user projects');
+    } catch (e) {
+        throw e;
+    }
+}
+
+main();
